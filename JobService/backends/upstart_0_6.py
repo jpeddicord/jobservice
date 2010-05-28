ae660eb5645f4ad8be6ced53a6a5548d27ce4494
Jacob Peddicord <jpeddicord@ubuntu.com>
2010-05-28 00:59:04 -0400

    Support instances in Upstart 0.6

--- JobService/backends/upstart_0_6.py
+++ JobService/backends/upstart_0_6.py
@@ -9,8 +9,8 @@ class ServiceBackend(ServiceBase):
         """
         Connect to Upstart's dbus service.
         """
-        
         self.namepath = {}
+        self.instances = {}
         self.bus = SystemBus()
         self.upstart = Interface(
             self.bus.get_object('com.ubuntu.Upstart', '/com/ubuntu/Upstart'),
@@ -20,11 +20,36 @@ class ServiceBackend(ServiceBase):
     def get_all_services(self):
         svclist = []
         for path in self.upstart.GetAllJobs():
-            job = self.bus.get_object('com.ubuntu.Upstart', path)
-            jobname = job.Get('com.ubuntu.Upstart0_6.Job', 'name',
-                              dbus_interface=PROPERTIES_IFACE)
+            job_obj = self.bus.get_object('com.ubuntu.Upstart', path)
+            job_name = job_obj.Get('com.ubuntu.Upstart0_6.Job', 'name',
+                                   dbus_interface=PROPERTIES_IFACE)
+            job = Interface(job_obj, 'com.ubuntu.Upstart0_6.Job')
             # cache the name to path mapping
-            self.namepath[jobname] = job
-            svclist.append(jobname)
+            self.namepath[job_name] = job
+            # get the instance(s) and their states
+            instances = job.GetAllInstances()
+            if instances:
+                self.instances[path] = []
+                for inst_path in instances:
+                    inst_obj = self.bus.get_object('com.ubuntu.Upstart',
+                                                   inst_path)
+                    inst_props = inst_obj.GetAll(
+                        'com.ubuntu.Upstart0_6.Instance',
+                        dbus_interface=PROPERTIES_IFACE
+                    )
+                    # cache this instance
+                    self.instances[path].append({inst_path: inst_props})
+                    if inst_props['name']:
+                        list_name = job_name + '/' + inst_props['name']
+                    # if there is no instance name, there's probably only one
+                    else:
+                        list_name = job_name
+                    svclist.append((
+                        list_name,
+                        inst_props['state'] == 'running'
+                    ))
+            # no running instances
+            else:
+                svclist.append((job_name, False))
         return svclist
         
