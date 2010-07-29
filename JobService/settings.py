50390a63b451a7fba24bf2fe379c55b59779369c
Jacob Peddicord <jpeddicord@ubuntu.com>
2010-07-29 17:56:06 -0400

    Add copyright headers.
    I really hate how much space these take up.

--- JobService/settings.py
+++ JobService/settings.py
@@ -1,3 +1,18 @@
+# This file is part of jobservice.
+# Copyright 2010 Jacob Peddicord <jpeddicord@ubuntu.com>
+#
+# jobservice is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# jobservice is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with jobservice.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
 from os import rename
